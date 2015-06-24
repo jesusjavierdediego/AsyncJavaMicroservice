4a908cb83c2efd38e37b4b236e792991fc84c3ab
jdediego <jdediego@fexco.com>
2015-06-24 08:45:36 +0100

    Fixed CORS filter

--- src/main/java/com/ms/utils/CORSFilter.java
+++ src/main/java/com/ms/utils/CORSFilter.java
@@ -12,8 +12,12 @@ public class CORSFilter implements ContainerResponseFilter {
 @Override
 public void filter(ContainerRequestContext requestContext,   ContainerResponseContext responseContext)
     throws IOException {
+        //http://www.codingpedia.org/ama/how-to-add-cors-support-on-the-server-side-in-java-with-jersey/
         responseContext.getHeaders().add("Access-Control-Allow-Origin","*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT. OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+
 
   }
 }
